57e8043295c7f0ddaa947f8730a986538dbe7224
alexpado <contact@alexpado.fr>
2024-02-10 03:19:03 +0100

    Added automatic wood stuff generator
    Because doing it by hand was starting to get on my nerves

---
+++ src/main/java/me/anisekai/AnisekaiHelper.java
@@ -0,0 +1,143 @@
+package me.anisekai;
+
+import com.mojang.datafixers.types.Func;
+import org.lwjgl.opengl.NVVertexArrayRange;
+
+import java.io.Console;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class AnisekaiHelper {
+
+    private static String map(String identifier, String wood, String content) {
+
+        return content.replace(String.format("##%s##", identifier), wood);
+    }
+
+    private static String mapWood(String wood, String content) {
+
+        return map("WOOD", wood, content);
+    }
+
+    private static String mapLog(String wood, String content) {
+
+        Map<String, String> overrides = new HashMap<>();
+        overrides.put("bamboo", "bamboo_block");
+        overrides.put("crimson", "crimson_stem");
+        overrides.put("warped", "warped_stem");
+
+        return map("LOG", overrides.getOrDefault(wood, String.format("%s_log", wood)), content);
+    }
+
+    private static String mapPlanks(String wood, String content) {
+
+        return map("PLKS", String.format("%s_planks", wood), content);
+    }
+
+    private static String mapSlabs(String wood, String content) {
+
+        return map("SLAB", String.format("%s_slab", wood), content);
+    }
+    private static String mapStairs(String wood, String content) {
+
+        return map("STRS", String.format("%s_stairs", wood), content);
+    }
+
+    private static String mapStrippedLog(String wood, String content) {
+
+        Map<String, String> overrides = new HashMap<>();
+        overrides.put("bamboo", "stripped_bamboo_block");
+        overrides.put("crimson", "stripped_crimson_stem");
+        overrides.put("warped", "stripped_warped_stem");
+        return map("STPD", overrides.getOrDefault(wood, String.format("stripped_%s_log", wood)), content);
+    }
+
+    private static String doMapping(String wood, String content) {
+
+        List<BiFunction<String, String, String>> mappers = Arrays.asList(
+                AnisekaiHelper::mapWood,
+                AnisekaiHelper::mapLog,
+                AnisekaiHelper::mapPlanks,
+                AnisekaiHelper::mapSlabs,
+                AnisekaiHelper::mapStairs,
+                AnisekaiHelper::mapStrippedLog
+        );
+
+        String newContent = content;
+        for (BiFunction<String, String, String> mapper : mappers) {
+            newContent = mapper.apply(wood, newContent);
+        }
+        return newContent;
+    }
+
+    private static void walk(File file, Consumer<File> onFileFound) {
+
+        if (file.isFile()) {
+            return;
+        }
+        for (File content : Objects.requireNonNull(file.listFiles())) {
+            if (content.isFile()) {
+                onFileFound.accept(content);
+            } else if (content.isDirectory()) {
+                walk(content, onFileFound);
+            }
+        }
+    }
+
+    private static List<String> woods = Arrays.asList(
+            "acacia",
+            "bamboo",
+            "birch",
+            "cherry",
+            "crimson",
+            "dark_oak",
+            "jungle",
+            "mangrove",
+            "oak",
+            "spruce",
+            "warped"
+    );
+
+
+    public static void main(String... str) throws Exception {
+
+        File input  = new File("generator-source");
+        File output = new File("src\\main\\resources");
+
+        BiFunction<File, String, File> toOuput = (file, name) -> {
+            File   parent = file.getParentFile();
+            String abs    = parent.getAbsolutePath();
+            String out    = abs.replace(input.getAbsolutePath(), output.getAbsolutePath());
+            File   dir    = new File(out);
+            dir.mkdirs();
+            return new File(dir, name);
+        };
+
+
+        List<File> files = new ArrayList<>();
+        walk(input, files::add);
+
+
+        for (File file : files) {
+            String content = Files.readString(file.toPath());
+
+            for (String wood : woods) {
+                String filename = String.format("%s_%s", wood, file.getName());
+                File   apply    = toOuput.apply(file, filename);
+
+                String newContent = doMapping(wood, content);
+                Files.writeString(apply.toPath(), newContent, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            }
+        }
+
+        System.out.printf("Deployed %s files for %s source files.", files.size() * woods.size(), files.size());
+    }
+}
